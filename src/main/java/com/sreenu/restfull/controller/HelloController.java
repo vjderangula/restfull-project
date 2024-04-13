277ebe28b963f6d4bcb96580e25d3fa1f69a8a79
Derangula Vijay <derangulavijay2011@gmail.com>
2024-04-13 07:39:52 +0530

    HelloWorld controller

---
+++ src/main/java/com/sreenu/restfull/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.sreenu.restfull.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    public String helloWorld(){
+
+        return "Hello World Kumar";
+
+    }
+}
