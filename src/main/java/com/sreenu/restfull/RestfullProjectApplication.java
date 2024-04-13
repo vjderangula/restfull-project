c22b45058556b35f45db0559ddbc1dbb181006b7
Derangula Vijay <derangulavijay2011@gmail.com>
2024-04-13 07:13:22 +0530

    Restfull project

---
+++ src/main/java/com/sreenu/restfull/RestfullProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sreenu.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreenu/restfull/RestfullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreenu.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
